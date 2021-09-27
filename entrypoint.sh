126100c2ce8cde75c68e0273fa4b60a562c13b7f
Mario Nevado <nevado.mario@gmail.com>
2021-09-27 11:10:19 +0200

    with login

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@ set -e
 
 mvndir=$1
 awsowner=$2
-#region=$3
-#aws_access_key_id=$4
-#aws_secret_access_key=$5
+region=$3
+aws_access_key_id=$4
+aws_secret_access_key=$5
 
-#aws configure set default.region $region
-#aws configure set aws_access_key_id $aws_access_key_id
-#aws configure set aws_secret_access_key $aws_secret_access_key
+aws configure set default.region $region
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
 
-~/.aws/credentials
 
 aws codeartifact get-authorization-token --domain dgp-mvn-artifacts-domain --domain-owner $awsowner --query authorizationToken --output text > /tmptoken
 
