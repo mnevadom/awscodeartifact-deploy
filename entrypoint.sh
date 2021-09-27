0ec84d154aa8b622c7dba8181a976a2d2356a1fb
Mario Nevado <nevado.mario@gmail.com>
2021-09-27 11:06:56 +0200

    creds

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,8 @@ awsowner=$2
 #aws configure set aws_access_key_id $aws_access_key_id
 #aws configure set aws_secret_access_key $aws_secret_access_key
 
+~/.aws/credentials
+
 aws codeartifact get-authorization-token --domain dgp-mvn-artifacts-domain --domain-owner $awsowner --query authorizationToken --output text > /tmptoken
 
 ls /
